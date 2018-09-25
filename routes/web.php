b7a83a46fc369247bf5d0d08fb00b2c1a9f86029
Johancillo <johanguiseppe@gmail.com>
2018-09-25 12:17:00 -0300

    fix: error route file web

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 Route::get('/solicitudes', 'TicketController@index')->name('tickets.index');
 
 Route::get('/solicitud/create','TicketController@create')->name('tickets.create');
-Route::post('/tickets/create','TicketController@store')->name('tickets.store');
+Route::post('/solicitud/create','TicketController@store')->name('tickets.store');
 
 Route::get('/solicitud/delete/{ticket}','TicketController@delete')->name('tickets.delete');
 Route::post('/solicitud/delete/{ticket}','TicketController@destroy')->name('tickets.destroy');
